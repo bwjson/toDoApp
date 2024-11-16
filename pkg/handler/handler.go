3f208fed55c1f36f76fa64ce49e114aac62cc918
bwjson <bwjson27@gmail.com>
2024-11-16 17:19:11 +0500

    swagger added

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	_ "github.com/bwjson/toDoApp/docs"
 	"github.com/bwjson/toDoApp/pkg/service"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
 type Handler struct {
@@ -15,6 +18,8 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	auth := router.Group("/auth")
 	{
